6f54d1428e562aec10e08aefe804bf8554c4b43d
yjm <125108268+YeomJaemoo@users.noreply.github.com>
2024-12-09 14:25:54 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+
+
+st.title("🚨_Reverse :red[Engineering]_Helper!_")
+st.write("## 😎튜토리얼을 시청 후 사용하면 좋습니다:)")
+
+video_url= "https://www.youtube.com/watch?v=0pD1yWTpUOY"
+st.video(video_url, format='video/mp4')
