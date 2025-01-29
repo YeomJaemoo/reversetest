b4251888d322da8cddea9d85844d9292761a8476
yjm <125108268+YeomJaemoo@users.noreply.github.com>
2025-01-29 14:02:55 +0900

    Update 3_부품 배경지우기(구글랜즈).py

--- pages/3_부품 배경지우기(구글랜즈).py
+++ pages/3_부품 배경지우기(구글랜즈).py
@@ -2,7 +2,6 @@ import streamlit as st
 from rembg import remove
 from PIL import Image
 from io import BytesIO
-import base64
 
 st.set_page_config(layout="wide", page_title="이미지 배경 제거", page_icon="😶‍🌫️")
 
@@ -14,21 +13,16 @@ MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 def convert_image(img):
     buf = BytesIO()
     img.save(buf, format="PNG")
-    byte_im = buf.getvalue()
-    return byte_im
+    return buf.getvalue()
 
 def fix_image(image_bytes):
-    if isinstance(image_bytes, BytesIO):
-        image = Image.open(image_bytes)
-    else:
-        image = Image.open(BytesIO(image_bytes))
+    image = Image.open(BytesIO(image_bytes))
     fixed = remove(image)
     return image, fixed
 
 def main():
     st.title("🤩Google Lens with Streamlit")
-    
-    # 카메라로 이미지를 입력받거나 파일을 업로드하기
+
     img_file_buffer = st.camera_input("📸사진찍기")
     uploaded_images = st.sidebar.file_uploader("이미지 업로드", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
 
@@ -38,33 +32,26 @@ def main():
         session_state.processed_images = []
         session_state.last_processed = 0
 
-    # Process Images 버튼을 누르기 전에 미리 이미지를 처리하기
     new_images = []
 
     if img_file_buffer is not None:
-        # 카메라로 찍은 이미지를 읽어오기
         image_bytes = img_file_buffer.getvalue()
-        if image_bytes is not None:
+        if len(image_bytes) > MAX_FILE_SIZE:
+            st.error("사진파일이 너무 큽니다. 5MB이하를 업로드하세요.")
+        else:
+            original_image, fixed_image = fix_image(image_bytes)
+            new_images.append((original_image, fixed_image))
+
+    if uploaded_images is not None:
+        for upload in uploaded_images[session_state.last_processed:]:
+            image_bytes = upload.read()
             if len(image_bytes) > MAX_FILE_SIZE:
                 st.error("사진파일이 너무 큽니다. 5MB이하를 업로드하세요.")
             else:
                 original_image, fixed_image = fix_image(image_bytes)
                 new_images.append((original_image, fixed_image))
 
-    if uploaded_images is not None:
-        # 업로드한 이미지를 읽어오기
-        for upload in uploaded_images[session_state.last_processed:]:
-            image_bytes = upload.read()
-
-            if image_bytes is not None:
-                if len(image_bytes) > MAX_FILE_SIZE:
-                    st.error("사진파일이 너무 큽니다. 5MB이하를 업로드하세요.")
-                else:
-                    original_image, fixed_image = fix_image(image_bytes)
-                    new_images.append((original_image, fixed_image))
-
     if st.sidebar.button("배경제거 하기 버튼"):
-        # Process Images 버튼을 누르면 새롭게 처리한 이미지만 추가하기
         session_state.processed_images.extend(new_images)
         session_state.last_processed = len(uploaded_images)
 
@@ -74,15 +61,12 @@ def main():
         col1.image(original_image)
         col2.write("Fixed Image :wrench:")
         col2.image(fixed_image)
-        st.sidebar.markdown("")
         st.sidebar.download_button(f"배경제거된 이미지 #{i+1}", convert_image(fixed_image), f"fixed_{i+1}.png", "image/png", key=f"download_button_{i}")
-    
+
     # 초기화 버튼을 추가하여 상태를 리셋하는 기능 추가
     if st.sidebar.button('초기화'):
-        # 처리된 이미지와 마지막 처리된 인덱스 초기화
-        session_state.processed_images = []
-        session_state.last_processed = 0
-        st.experimental_rerun()  # 상태가 변경되면 페이지를 새로고침
+        session_state.clear()  # 모든 상태 변수 초기화
+        st.rerun()  # 새로고침
 
 if __name__ == "__main__":
     main()
